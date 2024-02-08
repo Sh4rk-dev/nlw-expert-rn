287b146d9c8cc936f97b3dc9263aae300686c1f0
Renan <renanrapace13@gmail.com>
2024-02-08 12:40:10 -0300

    Creating Category Component and Adding on Homepage

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,40 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, FlatList } from "react-native";
+
+import { CATEGORIES } from "@/utils/data/products";
+
+import { Header } from "@/components/header";
+import { CategoryButton } from "@/components/category-button";
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    console.log(category);
+    setCategory(selectedCategory);
+  }
+
   return (
-    <View className="flex-1 pt-8">
-      <Text className="text-white text-2xl font-heading"> React Native</Text>
+    <View className="flex-1 pt-10">
+      <Header title="Faça o seu pedido" cartQuantityItems={2} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => {
+              handleCategorySelect(item);
+            }}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   );
 }
